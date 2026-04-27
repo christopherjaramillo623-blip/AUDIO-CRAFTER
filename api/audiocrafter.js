ab6e3a8eef8cadfb81f2fd8694f6c1edfcde1654
yourscoper <79878653+yourscoper@users.noreply.github.com>
2026-04-27 14:14:25 -0700

    Update audiocrafter.js

--- api/audiocrafter.js
+++ api/audiocrafter.js
@@ -1,12 +1,13 @@
 export default function handler(req, res) {
-    const accept = req.headers.accept || "";
+  const accept = req.headers.accept || "";
 
-    if (accept.includes("text/html")) {
-        res.writeHead(302, { Location: "/" });
-        return res.end();
-    }
+  if (accept.includes("text/html")) {
+    res.writeHead(302, { Location: "/" });
+    return res.end();
+  }
 
-    res.setHeader("Content-Type", "text/plain");
+  res.status(200);
+  res.setHeader("Content-Type", "text/plain");
 
-    res.send(`print("hello world")`);
+  return res.send('print("hello world")');
 }

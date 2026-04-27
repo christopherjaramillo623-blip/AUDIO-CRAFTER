2061b90196c9c625122ff959d1734d781f53e94d
AC-AUDIOCRAFTER <christopherjaramillo623@gmail.com>
2026-04-27 14:42:51 -0700

    Update audiocrafter.js

--- api/audiocrafter.js
+++ api/audiocrafter.js
@@ -1,17 +1,4 @@
 export default function handler(req, res) {
-  const accept = req.headers.accept || "";
-
-  if (accept.includes("text/html")) {
-    res.writeHead(302, { Location: "/" });
-    return res.end();
-  }
-
-  res.status(200);
   res.setHeader("Content-Type", "text/plain");
-
-  const lua = `
-    loadstring(game:HttpGet("https://audio-crafter.vercel.app/api/ac?key=ACMelody2024secretkey"))()
-`;
-
-  return res.send(lua);
+  return res.send('print("TEST OK")');
 }
